79172301bea3d3919a4563237e98d54ebf99d28c
Luis David Goyes Garces <luis.goyes117@gmail.com>
2020-01-26 11:59:23 -0500

    Initial commit

---
+++ Foil-card-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foil-card-animation
+//
+//  Created by Luis David Goyes Garces on 1/26/20.
+//  Copyright © 2020 Luis David Goyes Garces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
